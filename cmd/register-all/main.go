596414ae6895ad2013f293632fa4ca184aa6ae7e
diamondburned <diamond@libdb.so>
2024-04-06 22:54:21 -0700

    Add --json output to cmd/register-all

--- cmd/register-all/main.go
+++ cmd/register-all/main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"flag"
 	"log/slog"
 	"os"
 
 	"libdb.so/periph-gpioc/driver"
 )
 
+var jsonOutput bool
+
 func main() {
-	logHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		Level: slog.LevelDebug,
-	})
+	flag.BoolVar(&jsonOutput, "json", false, "output logs in JSON format")
+	flag.Parse()
+
+	opts := &slog.HandlerOptions{Level: slog.LevelDebug}
+
+	var logHandler slog.Handler
+	if jsonOutput {
+		logHandler = slog.NewJSONHandler(os.Stdout, opts)
+	} else {
+		logHandler = slog.NewTextHandler(os.Stdout, opts)
+	}
 
 	logger := slog.New(logHandler)
 	slog.SetDefault(logger)
